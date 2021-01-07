f3df59f10f7b2a1511013b1a86445bd30910bc3a
Omar <y0028613702420400@gmail.com>
2021-01-07 17:40:56 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Credits/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import Credits from './components/Credits.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
+    Credits
   }
 }
 </script>
@@ -19,7 +19,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/Credits.vue
@@ -0,0 +1,46 @@
+<template>
+  <div >
+      
+    <div class="mx-10 xl:max-w-screen-xl m-auto text-gray-600">  
+        <div v-for="credit in mycredits" :key="credit"> 
+            <div class="flex justify-between">
+                <h3 class="text-2xl my-2 mx-6">{{credit.簡述}}</h3>
+                <div class="flex align-middle justify-start w-40">
+                    <h3 class="text-2xl mx-6">{{credit.分類}}</h3>
+                    <button  v-if="credit.案由 != ''" @click="credit.isOpen = !credit.isOpen"  class="text-gray-500 hover:text-black focus:text-black pb-3">
+                        <svg  class="h-6 w-6 fill-current" viewBox="-2.5 -5 75 60" preserveAspectRatio="none">
+                            <path d="M0,0 l35,50 l35,-50" fill-rule="evenodd" stroke-width="5" />
+                        </svg>
+                    </button>
+                </div>
+
+            </div>
+            <div v-if="credit.isOpen" class="mx-4 mb-6">
+                <p class="text-gray-400"> {{credit.日期}} </p>
+                <p> {{credit.案由}} </p>
+            </div>
+        </div>
+    </div>
+      
+  </div>
+</template>
+
+<script>
+import credits from '../../public/csvjson.json'
+export default {
+    name: "Credits",
+    data(){
+        return{
+            mycredits: credits,
+            
+        }
+    },
+
+    
+    
+}
+</script>
+
+<style>
+
+</style>
